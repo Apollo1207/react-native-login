7ee87919d8da7c55e844c5547ea6790f5ebca919
Apollo1207 <fednyakillya12072001@gmail.com>
2020-05-13 16:32:48 +0300

    a

---
+++ src/core/theme.js
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#5d9184',
+    secondary: '#414757',
+    error: '#f13a59',
+  },
+};
